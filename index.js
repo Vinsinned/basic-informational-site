4cea177d81b31d4d05aa1909e3feba4ed66a5db5
vinson <dagger1245653@gmail.com>
2022-02-26 08:42:16 -0800

    add start page

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const http = require('http');
-var fs = require('fs');
+const fs = require('fs');
+const url  = require('url');
 
 const host = 'localhost';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-		fs.readFile('./index.html', function (err, html) {
+	res.writeHeader(200, { "Content-Type": "text/html" });
+	if (req.url == '/') {
+		fs.readFile('./index.html', function (err, data) {
 			if (err) {
+				response.writeHead(404);
 				throw err; 
 			}  
 			else {
-				res.writeHeader(200, {"Content-Type": "text/html"});  
-				res.write(html);  
+				res.write(data);  
 				res.end(); 
 			}
 		});
+	} else {
+		res.end('<h1>Hello im vinsonnn</h1>'); 
+	}
 }).listen(port);
