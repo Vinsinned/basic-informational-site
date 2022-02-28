f56662fcb9d1814a26406aefb3f4edff8c5628af
vinson <dagger1245653@gmail.com>
2022-02-27 19:02:12 -0800

    do a simple express rewrite on project

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const fs = require('fs');
-const url  = require('url');
 
 const host = 'localhost';
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-	res.writeHeader(200, { "Content-Type": "text/html" });
-	if (req.url == '/') {
-		fs.readFile('./index.html', function (err, data) {
-			if (err) {
-				response.writeHead(404);
-				throw err; 
-			}  
-			else {
-				res.write(data);  
-				res.end(); 
-			}
-		});
-	} else if (req.url == '/about') {
-		fs.readFile('./about.html', function (err, data) {
-			if (err) {
-				response.writeHead(404);
-				throw err; 
-			}  
-			else {
-				res.write(data);  
-				res.end(); 
-			}
-		});
-	} else if (req.url == '/contact-me') {
-		fs.readFile('./contact-me.html', function (err, data) {
-			if (err) {
-				response.writeHead(404);
-				throw err; 
-			}  
-			else {
-				res.write(data);  
-				res.end(); 
-			}
-		});
-	} else {
-		fs.readFile('./404.html', function (err, data) {
-			if (err) {
-				response.writeHead(404);
-				res.end('<h1>ERROR 404</h1>')
-				throw err; 
-			}  
-			else {
-				res.write(data);  
-				res.end(); 
-			}
-		});
-	}
-}).listen(port);
+app.get('/', (req, res) => {
+	fs.readFile('./index.html', function (err, data) {
+		if (err) {
+			response.writeHead(404);
+			throw err;
+		}
+		else {
+			res.write(data);
+			res.end();
+		}
+	});
+});
+app.get('/about', (req, res) => {
+	fs.readFile('./about.html', function (err, data) {
+		if (err) {
+			response.writeHead(404);
+			throw err; 
+		}  
+		else {
+			res.write(data);  
+			res.end(); 
+		}
+	});
+});
+app.get('/contact-me', (req, res) => {
+	fs.readFile('./contact-me.html', function (err, data) {
+		if (err) {
+			response.writeHead(404);
+			throw err; 
+		}  
+		else {
+			res.write(data);  
+			res.end(); 
+		}
+	});
+});
+app.get('*', (req, res) => {
+	fs.readFile('./404.html', function (err, data) {
+		if (err) {
+			response.writeHead(404);
+			res.end('<h1>ERROR 404</h1>')
+			throw err; 
+		}  
+		else {
+			res.write(data);  
+			res.end(); 
+		}
+	});
+});
+
+app.listen(port, function (err) {
+    if(err){
+        console.log("error while starting server");
+    }
+    else{
+        console.log("server has been started at port "+port);
+    }
+})
